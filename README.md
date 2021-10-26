3a77ea292154096c017a28f79972a75ea757709f
PLMQuaterback <marc.weidner@protonmail.ch>
2021-10-26 15:03:15 +0200

    inch umrechner erstellt

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 this is for me where I try to learn GitHub and python
 
 at the beginning it startet to do this as private
+
+#26.10.2021 started with this
---
+++ inch_umrechner.py
@@ -0,0 +1,15 @@
+#inch umrechner für werteingabe eines anwenders
+inch = 2.54 #1 Inch sind 2.54 Zentinmer
+
+#Eingabe des Inch Wertes
+print("Bitte geben Sie einen Wert in inch ein")
+xi = float(input())
+
+#Umrechnung
+xcm = xi * inch
+
+#Ausgabe 
+print(xi, "Inch sind", xcm, "cm")
+
+
+#berechnung 
